eb0f4996310ac1d64f250f3ad84ffb6040545917
unknown <omilude.omololamercy@gmail.com>
2022-11-16 22:05:07 +0100

    string

---
+++ string_assign.js
@@ -0,0 +1,21 @@
+let statement = "A long time ago in a country" + 
+" far away from Nigeria..."
+console.log(statement)
+
+console.log(statement.slice(0, 1));
+
+console.log(statement.substring (1,0));
+
+console.log(statement.substr(0,1));
+
+
+console.log(statement.slice(12,15));
+
+console.log(statement.substring(11,15));
+
+console.log(statement.substr(12,3));
+
+
+console.log(statement.slice(21,28));
+
+console.log(statement.substring ())
